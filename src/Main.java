0660168109cb8e5af0362fee96527686d0f90c4f
Ricardo de Moraes Gonçalves <54293143+RicardoMoraesG@users.noreply.github.com>
2022-10-07 20:33:57 -0300

    Classes Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(2);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descrição curso js");
+        curso2.setCargaHoraria(3);
+
+        Curso curso3 = new Curso();
+        curso3.setTitulo("curso springBoot");
+        curso3.setDescricao("descrição curso springBoot");
+        curso3.setCargaHoraria(4);
+
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Mentoria de Java");
+        mentoria.setData(LocalDate.now());
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(curso3);
+
+
     }
 }
